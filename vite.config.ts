e277fa0a0e21edbdbc8c48d9f85bbfbf35348470
Callum Flack <callumflack@gmail.com>
2026-01-30 13:03:45 +1000

    add shadcn, configure tailwind

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { execSync } from 'child_process'
+import path from 'path'
+import tailwindcss from '@tailwindcss/vite'
 
 // Get git commit hash
 const commitHash = execSync('git rev-parse --short HEAD').toString().trim()
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
   define: {
     __COMMIT_HASH__: JSON.stringify(commitHash),
   },

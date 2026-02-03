90f481c9953556c872cf13adce9b55e9708ce374
Callum Flack <callumflack@gmail.com>
2026-02-03 14:10:23 +1000

    delete legacy grant-flow-sections

--- src/pages/grant-flow-sections/index.ts
+++
@@ -1,5 +0,0 @@
-export { GrantLoadingState } from './GrantLoadingState';
-export { GrantAuthRequiredState } from './GrantAuthRequiredState';
-export { GrantErrorState } from './GrantErrorState';
-export { GrantSuccessState } from './GrantSuccessState';
-export { GrantConsentState } from './GrantConsentState';

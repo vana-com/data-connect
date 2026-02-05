78941a1590db8514c44df4e1df0a7ad215143f68
Callum Flack <callumflack@gmail.com>
2026-02-05 21:48:59 +1000

    Update dc-icon.tsx

--- src/components/icons/dc-icon.tsx
+++ src/components/icons/dc-icon.tsx
@@ -6,16 +6,16 @@ export const DcIcon = (props: SvgIconProps) => {
       <path
         d="M125 175V108.333C125 106.123 124.122 104.004 122.559 102.441C120.996 100.878 118.877 100 116.667 100H83.3333C81.1232 100 79.0036 100.878 77.4408 102.441C75.878 104.004 75 106.123 75 108.333V175"
         stroke="url(#paint0_linear_2103_9686)"
-        stroke-width="15.7895"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="15.7895"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
       <path
         d="M25 83.3347C24.9994 80.9103 25.5278 78.5149 26.5482 76.3157C27.5687 74.1165 29.0566 72.1664 30.9083 70.6014L89.2417 20.6014C92.2499 18.059 96.0613 16.6641 100 16.6641C103.939 16.6641 107.75 18.059 110.758 20.6014L169.092 70.6014C170.943 72.1664 172.431 74.1165 173.452 76.3157C174.472 78.5149 175.001 80.9103 175 83.3347V158.335C175 162.755 173.244 166.994 170.118 170.12C166.993 173.245 162.754 175.001 158.333 175.001H41.6667C37.2464 175.001 33.0072 173.245 29.8816 170.12C26.7559 166.994 25 162.755 25 158.335V83.3347Z"
         stroke="url(#paint1_linear_2103_9686)"
-        stroke-width="15.7895"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="15.7895"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
       <defs>
         <linearGradient
@@ -27,7 +27,10 @@ export const DcIcon = (props: SvgIconProps) => {
           gradientUnits="userSpaceOnUse"
         >
           <stop style={{ stopColor: "var(--logo-stop-0, #888888)" }} />
-          <stop offset="1" style={{ stopColor: "var(--logo-stop-1, #161616)" }} />
+          <stop
+            offset="1"
+            style={{ stopColor: "var(--logo-stop-1, #161616)" }}
+          />
         </linearGradient>
         <linearGradient
           id="paint1_linear_2103_9686"
@@ -38,7 +41,10 @@ export const DcIcon = (props: SvgIconProps) => {
           gradientUnits="userSpaceOnUse"
         >
           <stop style={{ stopColor: "var(--logo-stop-0, #888888)" }} />
-          <stop offset="1" style={{ stopColor: "var(--logo-stop-1, #161616)" }} />
+          <stop
+            offset="1"
+            style={{ stopColor: "var(--logo-stop-1, #161616)" }}
+          />
         </linearGradient>
       </defs>
     </SvgComponent>
